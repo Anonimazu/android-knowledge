d83d64d60f661860bd46177982026d01938acd9e
Anonimazu <monthon.chat@gmail.com>
2022-12-20 18:46:21 +0700

    add dependency

--- app/src/main/java/com/anonimazu/android_knowledge/MainActivity.kt
+++ app/src/main/java/com/anonimazu/android_knowledge/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.anonimazu.android_knowledge
+package com.anonimazu.android_knowledge // ktlint-disable package-name
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
